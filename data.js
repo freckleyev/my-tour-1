925988de9637991b1215600955cb072856dfa816
freckleyev <evgeniyvladimirovych@gmail.com>
2025-02-03 16:55:50 +0100

    Version 2 upload

--- data.js
+++ data.js
@@ -24,6 +24,51 @@ var APP_DATA = {
       ],
       "faceSize": 1500,
       "initialViewParameters": {
+        "yaw": -1.1883728563347518,
+        "pitch": -0.7520334317449926,
+        "fov": 1.4613077086558952
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9945909464131013,
+          "pitch": 0.04763964450995495,
+          "rotation": 5.497787143782138,
+          "target": "1-360_top_16_master_bedroom"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.9769629509000985,
+          "pitch": 0.016274073713542947,
+          "title": "fghjgfhkjg",
+          "text": "dfnhfxgjgdh"
+        }
+      ]
+    },
+    {
+      "id": "1-360_top_16_master_bedroom",
+      "name": "360_Top_16_Master_Bedroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
         "pitch": 0,
         "yaw": 0,
         "fov": 1.5707963267948966
@@ -36,7 +81,7 @@ var APP_DATA = {
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
-    "fullscreenButton": false,
-    "viewControlButtons": false
+    "fullscreenButton": true,
+    "viewControlButtons": true
   }
 };
